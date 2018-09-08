9c83882e6840d0fd2f16855a3742fe209e36f9a5
chiefmb <35791547+chiefmb@users.noreply.github.com>
2018-09-08 12:01:10 +0430

    Add project files.

---
+++ Project001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
